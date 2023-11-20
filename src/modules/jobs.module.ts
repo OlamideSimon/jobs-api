b47e686f9cc5223cf2fdfc44c1cb18e239d28903
Olamide <simonadepetoye@gmail.com>
2023-11-20 22:57:50 +0100

    --empty

--- src/modules/jobs.module.ts
+++ src/modules/jobs.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JobsController } from 'src/controllers/jobs.controller';
 import { Jobs } from 'src/entities/jobs.entity';
@@ -7,6 +8,6 @@ import { JobService } from 'src/services/jobs.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Jobs])],
   controllers: [JobsController],
-  providers: [JobService],
+  providers: [JobService, JwtService],
 })
 export class JobsModule {}
